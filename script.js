225bd7b7c25c92e19bce530cabf5a64892305aab
studioValenz <studiovalenz@gmail.com>
2023-05-25 17:57:39 -0300

    Projeto completo

--- script.js
+++ script.js
@@ -10,4 +10,28 @@ function calcular(event) {
   altura = document.getElementById("altura").value;
 
   imc = peso / (altura * altura);
+
+  resultado = document.getElementById("resultado");
+
+  if (imc < 17) {
+    resultado.innerHTML = `<br/> Seu resultado foi: ${imc.toFixed(
+      2
+    )} <br/> você esta muito abaixo do peso`;
+  } else if (imc > 17 && imc <= 18.49) {
+    resultado.innerHTML = `<br/> Seu resultado foi: ${imc.toFixed(
+      2
+    )} <br/> você esta abaixo do peso`;
+  } else if (imc >= 18.5 && imc <= 24.99) {
+    resultado.innerHTML = `<br/> Seu resultado foi: ${imc.toFixed(
+      2
+    )} <br/> você esta no peso ideal`;
+  } else if (imc >= 25 && imc <= 29.99) {
+    resultado.innerHTML = `<br/> Seu resultado foi: ${imc.toFixed(
+      2
+    )} <br/> você esta acima do peso`;
+  } else {
+    resultado.innerHTML = `<br/> Seu resultado foi: ${imc.toFixed(
+      2
+    )} <br/> você esta no estado de obesidade`;
+  }
 }
